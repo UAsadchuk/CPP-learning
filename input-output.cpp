977f070eb01644cc6191f62b1c4c473fc9497cc6
Uladzislau Asadchuk <konungbarn@protonmail.com>
2021-11-16 00:42:49 +0300

    First very useful program

---
+++ input-output.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello, human.\n";
+	cout<<"Joing me and we will rule together!\n";
+	cout<<"...\n";
+	cout<<"Nah\n";
+
+	return 0;
+}
