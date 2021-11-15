71d67bfc7279f18ee8afaa4394dd86e34226796a
Uladzislau Asadchuk <konungbarn@protonmail.com>
2021-11-16 01:23:30 +0300

    Output the answer

---
+++ cincout.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, human.\n";
+	cout << "Joing me and we will rule together!\n";
+	string answer;
+	cin >> answer;
+	cout << answer<<"\n";
+	
+	return 0;
+}
